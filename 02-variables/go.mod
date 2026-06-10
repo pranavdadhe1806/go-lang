c9625143bd72bbaae5e2fea5608a215a942b96e1
Pranav Dadhe <pranavdadhe1806@gmail.com>
2026-06-10 23:29:55 +0530

    02 Variables

---
+++ 02-variables/go.mod
@@ -0,0 +1,3 @@
+module 02-variables
+
+go 1.26.2
---
+++ 02-variables/variables.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 69
+	var b float64 = 69.69
+	var c string = "Hello"
+	var d rune = '🔥' // stores single character of UTF-8
+	var e byte = 'b' // stores single character of ASCII
+	var f bool = true
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+}
